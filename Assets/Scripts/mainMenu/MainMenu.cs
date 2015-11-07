fb39d030715c06df2b782d755ad652a02221b0be
jflorimo <jflorimo@student.42.fr>
2015-11-07 19:58:56 +0100

    menu

---
+++ Assets/Scripts/mainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Image background;
+
+	private float time = 0f;
+	private float step = 0.001f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color tmp = background.color;
+		if (tmp.g >= 0.6f || tmp.g < 0f)
+			step *= -1;
+		tmp.g += step;
+		background.color = tmp;
+	}
+}
