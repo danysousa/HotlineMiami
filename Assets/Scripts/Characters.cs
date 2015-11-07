0ff5cfd6323e0c5fa857982d4fbd6f026553f96b
Dany SOUSA <dsousa@e1r11p2.42.fr>
2015-11-07 11:47:01 +0100

    Player base

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Characters : MonoBehaviour
+{
+	public float			speedMove;
+
+	protected Rigidbody2D	_rbody;
+
+	protected void		init()
+	{
+		this._rbody = GetComponent<Rigidbody2D>();
+	}
+
+	protected void		move(Vector2 direction)
+	{
+		this._rbody.velocity = direction;
+	}
+
+	protected void		updateCharacters()
+	{
+			this._rbody.velocity *= 0.9f;
+	}
+}
