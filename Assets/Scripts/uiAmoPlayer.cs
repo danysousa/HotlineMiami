439aa6fc90a9c02316e2aa32d236e60398df56d4
NormanSchilling <norman.schilling.ns@gmail.com>
2015-11-08 12:36:23 +0100

    menu Amo

--- Assets/Scripts/uiAmoPlayer.cs
+++ Assets/Scripts/uiAmoPlayer.cs
@@ -4,25 +4,26 @@ using UnityEngine.UI;
 
 public class uiAmoPlayer : MonoBehaviour {
 
-//	public Player			player;
-//	public GameObject		weapon;
-//	public GameObject		amo;
-//
-//	private Text			_weaponText;
-//	private Text			_amoText;
+	public Player			player;
+	public GameObject		weapon;
+	public GameObject		amo;
+
+	private Text			_weaponText;
+	private Text			_amoText;
 
 	void Start ()
 	{
-//		_weaponText = this.weapon.GetComponent<Text> ();
-//		_amoText = this.amo.GetComponent<Text> ();
+		_weaponText = this.weapon.GetComponent<Text> ();
+		_amoText = this.amo.GetComponent<Text> ();
 	}
 
 	void Update () {
-//		if (this.player.equiped) {
-//			Debug.Log (this.player.weapon.name);
-//			Debug.Log (this.player.weapon.amo.ToString());
-//			_weaponText.text = this.player.weapon.name;
-//			_amoText.text = this.player.weapon.amo.ToString();
-//		}
+		if (this.player.equiped) {
+			_weaponText.text = this.player.weapon.name;
+			_amoText.text = this.player.weapon.amo.ToString ();
+		} else {
+			_weaponText.text = "No Weapon";
+			_amoText.text = "0";
+		}
 	}
 }
