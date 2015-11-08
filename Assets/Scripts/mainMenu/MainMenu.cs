27a1fe7d7161127a3ccdb8ab9040913eccf8c6f8
jflorimo <jflorimo@student.42.fr>
2015-11-08 13:00:03 +0100

    main menu done

--- Assets/Scripts/mainMenu/MainMenu.cs
+++ Assets/Scripts/mainMenu/MainMenu.cs
@@ -13,6 +13,9 @@ public class MainMenu : MonoBehaviour {
 	private float stepRotation = 0.005f;
 	private bool left = true;
 
+	public Text subtilteText;
+	private bool subtile = true;
+
 	public Vector3[] list;
 
 	public Button	start;
@@ -23,6 +26,8 @@ public class MainMenu : MonoBehaviour {
 	public Text	exit1;
 	public Text	exit2;
 
+	public Image eiffel;
+
 
 	private float time = 0f;
 	private float step = 0.001f;
@@ -30,6 +35,7 @@ public class MainMenu : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		initialPositionTitle2 = title2.transform.position;
+		StartCoroutine(subtitle());
 	}
 
 	private void modifyBackground()
@@ -64,6 +70,19 @@ public class MainMenu : MonoBehaviour {
 			pos.x = pos.x - 0.02f;
 		title2.transform.position = pos;
 		yield return new WaitForSeconds(0.1f);
+	}
+
+	private IEnumerator        subtitle()
+	{
+		while (true) {
+			if (subtile)
+				subtilteText.text = "Error: invalid instruction";
+			else
+				subtilteText.text = "Error: invalid instruction _";
+			subtile = !subtile;
+
+			yield return new WaitForSeconds (1f);
+		}
 	}
 
 	private void modifyTitle()
@@ -114,10 +133,28 @@ public class MainMenu : MonoBehaviour {
 		exit2.text = "Exit";
 	}
 
+	public void eiffelMove()
+	{
+
+		Vector3 scale = eiffel.transform.localScale;
+
+		scale.x += 0.08f;
+		scale.y += 0.08f;
+
+
+		if (scale.x >= 2f) {
+			scale.x = 0.001f;
+			scale.y = 0.001f;
+		}
+
+		eiffel.transform.localScale = scale;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		modifyBackground ();
 		StartCoroutine(this.titleEffect());
+		eiffelMove ();
 		modifyTitle ();
 	}
 }
